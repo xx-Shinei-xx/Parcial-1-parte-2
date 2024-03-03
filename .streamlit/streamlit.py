170554543cac35b76654eb0678a4514a66f16fbe
Shawn <160689002+xx-Shinei-xx@users.noreply.github.com>
2024-03-03 15:31:41 -0600

    Update streamlit.py

--- .streamlit/streamlit.py
+++ .streamlit/streamlit.py
@@ -44,7 +44,8 @@ def valores_de_n_y_p():
     st.write("Esta app fue creada con el propósito de mostrar distribuciones binomiales. La distribución binomial modela la probabilidad discreta del número de éxitos en una serie de ensayos independientes, donde cada ensayo tiene una probabilidad fija de éxito. ")
 
     # #para los valores de n (número de experimentos realizados)
-    n = st.number_input("Por favor ingrese un valor de n menor que 100:  ", min_value=1, max_value=100, value=1, step=1)
+    #n = st.number_input("Por favor ingrese un valor de n menor que 100:  ", min_value=1, max_value=100, value=1, step=1)
+  n = st.slider('Number of Trials (n)', 1, 100, 1)
      #para los valores de p (probabilidad)
     p = st.number_input("Por favor ingrese un valor de p, tal que p es mayor que 0 pero menor que 1 : ", min_value=0.0, max_value=1.0, value=0.5, step=0.01)
 
