98a4baacaeebf9a15e35e8fe62ace5c89850d6a4
Shawn <160689002+xx-Shinei-xx@users.noreply.github.com>
2024-03-03 16:13:14 -0600

    Update streamlit.py

--- .streamlit/streamlit.py
+++ .streamlit/streamlit.py
@@ -95,16 +95,6 @@ def valores_de_n_y_p():
          distribucion_binomial(int(n), p)
 
 
-txt = st.text_area(
-    "Text to analyze",
-    "It was the best of times, it was the worst of times, it was the age of "
-    "wisdom, it was the age of foolishness, it was the epoch of belief, it "
-    "was the epoch of incredulity, it was the season of Light, it was the "
-    "season of Darkness, it was the spring of hope, it was the winter of "
-    "despair, (...)",
-    )
-
-st.write(f'You wrote {len(txt)} characters.')
 
 if __name__ == "__main__":
     valores_de_n_y_p()
