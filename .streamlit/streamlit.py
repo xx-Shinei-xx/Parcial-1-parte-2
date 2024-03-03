c790943ade724ad4f0f1bbc543f6823c2b02dc20
Shawn <160689002+xx-Shinei-xx@users.noreply.github.com>
2024-03-03 15:54:42 -0600

    Update streamlit.py

--- .streamlit/streamlit.py
+++ .streamlit/streamlit.py
@@ -50,9 +50,6 @@ def valores_de_n_y_p():
  #una breve descrippcion del programa
     st.write("Esta app fue creada con el propósito de mostrar distribuciones binomiales. La distribución binomial modela la probabilidad discreta del número de éxitos en una serie de ensayos independientes, donde cada ensayo tiene una probabilidad fija de éxito. ")
 
-st.latex(r'''
-    P(X = k) = \binom{n}{k} \cdot p^k \cdot (1 - p)^{n - k}
-    ''')
 
     
     # #para los valores de n (número de experimentos realizados)
