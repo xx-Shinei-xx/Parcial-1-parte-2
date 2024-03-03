c12eb1fe1a835b56cf839569ed3ff0e2f516259e
Shawn <160689002+xx-Shinei-xx@users.noreply.github.com>
2024-03-03 16:40:31 -0600

    Update streamlit.py

--- .streamlit/streamlit.py
+++ .streamlit/streamlit.py
@@ -78,12 +78,8 @@ def valores_de_n_y_p():
         """,
         unsafe_allow_html=True
     )
-    txt = st.text_area(
-        "Escogí este widget porque quería uno que no solo fuera funcional, "
-        "sino que también tuviera una estética llamativa y para que fuera más fácil de usar. "
-        "Después de investigar y probar varias opciones, me decidí por este tipo de widget "
-        "porque me gustó y era más fácil de usar.",
-        height=100, # Adjust height as needed
+    txt = st.text("Escogí este widget porque quería uno que no solo fuera funcional, sino que también tuviera una estética llamativa y para que fuera más fácil de usar. Después de investigar y probar varias opciones, me decidí por este tipo de widget porque me gustó y era más fácil de usar.
+        height=100, 
         key='textarea'
     )
 
